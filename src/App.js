ae1a208bb668e847069e91443fe4dcb2652eb07f
cryptomehedi <classicalmehedi@gmail.com>
2022-04-04 16:38:37 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Component/About/About';
+import Blog from './Component/Blog/Blog';
+import Dashboard from './Component/Dashboard/Dashboard';
 import Header from './Component/Header/Header';
 import HomePage from './Component/HomePage/HomePage';
+import PageNotFound from './Component/PageNotFound/PageNotFound';
 import Reviews from './Component/Reviews/Reviews';
 
 function App() {
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
     </div>
   );
